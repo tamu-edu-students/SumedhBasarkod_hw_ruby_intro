b918be7fc9373d31d4d2885100b3ebbe66010bb4
sumedhpb <sumedhpb8@gmail.com>
2022-09-10 19:51:47 -0500

    Update the Gemfile to 3.1.2 and ...
    ... Finish the section 1 code stubs

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,15 +3,25 @@
 # Part 1
 
 def sum(arr)
-  # YOUR CODE HERE
+  # add elements of the array. Start with sum as 0
+  arr.inject(0, :+)
 end
 
 def max_2_sum(arr)
-  # YOUR CODE HERE
+  # Sort the array in non-descending order. Take the last two and add.
+  arr.sort.last(2).inject(0, :+)
 end
 
 def sum_to_n?(arr, number)
-  # YOUR CODE HERE
+  # Store combinations of 2
+  arr_2 = arr.combination(2)
+  # Find with a condition
+  ans = arr_2.find{|x,y| x+y==number}
+  if(ans) 
+    return true
+  else 
+    return false
+  end
 end
 
 # Part 2
